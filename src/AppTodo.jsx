55e2c09e357bb446e936a83dce6b2a3bc11cfc65
cristian quintriqueo <sircristian28@gmail.com>
2025-01-17 18:40:37 -0300

    The desktop version was added

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -1,5 +1,5 @@
 
-import { useReducer, useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import './styles.css'
 import imgDark from './assets/images/icon-moon.svg'
 import imgLight from './assets/images/icon-sun.svg'
@@ -23,15 +23,24 @@ const initialState = [
     }
 ];
 
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || initialState;
+}
+
 export const AppTodo = () => {
     const [darkMode, setDarkMode] = useState(false);
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
     const [filter, setFilter] = useState(Filters.all);
 
     const handleDarkMode = () => {
         setDarkMode(!darkMode);
         document.body.classList.toggle('dark-mode');
     };
+
+    useEffect(() => {
+      JSON.stringify(localStorage.setItem('todos', JSON.stringify(todos)));
+    }, [todos])
+    
 
     const handleNewTodo = (todo) => {
         dispatch({
@@ -86,7 +95,7 @@ export const AppTodo = () => {
                 <header className='app-header'>
                     <h1>Todo</h1>
                     <button className='switch-mode' onClick={handleDarkMode}>
-                        <img src={darkMode ? imgDark : imgLight} alt="darkmode-img" />
+                    <img src={darkMode ? imgDark : imgLight} alt="darkmode-img" />
                     </button>
                 </header>
                 <AddTodo newTodo={handleNewTodo}/>
@@ -94,14 +103,21 @@ export const AppTodo = () => {
                     <TodoList todo={filteredTodos} onDelete={handleDeleteTodo} onToggle={handleToggleTodo} />
                     <li className='todo-footer'>
                         <p>{todosPending} items left</p>
+
+                        <div className="footer-item-block">
+                        <button className={filter === Filters.all ? 'footer-item-selected': 'footer-item-no-selected'} onClick={() => setFilter(Filters.all)}>All</button>
+                        <button className={filter === Filters.active ? 'footer-item-selected': 'footer-item-no-selected'} onClick={() => setFilter(Filters.active)}>Active</button>
+                        <button className={filter === Filters.completed ? 'footer-item-selected': 'footer-item-no-selected'} onClick={() => setFilter(Filters.completed)}>Completed</button>
+                        </div>
+                        
                         <span onClick={handleDeleteAll}>Clear Completed</span>
                     </li>
                 </ul>
                 <ul>
                     <li className="footer-item">
-                        <button onClick={() => setFilter(Filters.all)}>All</button>
-                        <button onClick={() => setFilter(Filters.active)}>Active</button>
-                        <button onClick={() => setFilter(Filters.completed)}>Completed</button>
+                        <button className={filter === Filters.all ? 'footer-item-selected': 'footer-item-no-selected'} onClick={() => setFilter(Filters.all)}>All</button>
+                        <button className={filter === Filters.active ? 'footer-item-selected': 'footer-item-no-selected'} onClick={() => setFilter(Filters.active)}>Active</button>
+                        <button className={filter === Filters.completed ? 'footer-item-selected': 'footer-item-no-selected'} onClick={() => setFilter(Filters.completed)}>Completed</button>
                     </li>
                 </ul>
                 <p className='reorder'>Drag and drop to reorder list</p>
